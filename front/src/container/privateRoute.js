802521a78b391996a3a1064b91c573d436e0c2d9
sana <saanounisana34@gmail.com>
2022-05-23 10:01:01 +0100

    update-project 2

---
+++ front/src/container/privateRoute.js
@@ -0,0 +1,29 @@
+import React from 'react';  
+import { Navigate, useLocation, } from 'react-router-dom';
+import { checkCookie } from '../utils/cookies';
+
+// const PrivateRoute = ({ component: Component, ...rest }) => (  
+//   <Route { ...rest } render={props => (
+//     checkCookie() !== null ? (
+//       <Component { ...props } />
+//     ) : (
+//       <Navigate to={{
+//           pathname: '/',
+//           state: { from: props.location }
+//         }}
+//       />
+//     )
+//   )} />
+// );
+
+const PrivateRoute = ({ children }) => {
+	const location = useLocation();
+
+	if (checkCookie() == null) {
+		return <Navigate to='/' state={{ from: location }} />;
+	}
+
+  return children;
+}
+
+export default PrivateRoute;
